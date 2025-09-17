e6e567bf695ad323422e80702f3b96d6cf1ae322
Rohaansandhu <rohaan.sandhu@gmail.com>
2025-09-16 22:39:01 -0700

    Fix regex in script

--- get_voted_bills.py
+++ get_voted_bills.py
@@ -4,27 +4,24 @@ import subprocess
 from pathlib import Path
 
 # Path to the congress repo data directory
-CONGRESS_DATA_DIR = Path("congress/data/119/votes/2025")
-
-# Output directory for member-organized data
-OUTPUT_DIR = Path("data/bill_info")
-OUTPUT_DIR.mkdir(exist_ok=True)
+CONGRESS_DATA_DIR = Path("data/119/votes/2025")
 
 def fetch_bill_status(bill_id: str, congress: str):
+    # filter regex
+    bill_id_regex = f"^{bill_id}$"
+
     # Build the command as a list of arguments
     cmd = [
         "usc-run",
         "govinfo",
         "--bulkdata=BILLSTATUS",
         f"--congress={congress}",
-        f"--filter={bill_id}"
+        f"--filter={bill_id_regex}"
     ]
 
     try:
         # Run the command
         result = subprocess.run(cmd, capture_output=True, text=True, check=True)
-        # print("STDOUT:", result.stdout)
-        # print("STDERR:", result.stderr)
     except subprocess.CalledProcessError as e:
         print("Command failed:", e)
 
@@ -32,7 +29,7 @@ def build_bill_id(bill: dict) -> str:
     """
     Turn a bill object into a filter string for usc-run.
     Example: {"congress":119,"number":242,"type":"hres"} 
-             -> "BILLS-119hres242"
+             -> "BILLSTATUS-119hres242"
     """
     congress = bill["congress"]
     number = bill["number"]
@@ -41,7 +38,7 @@ def build_bill_id(bill: dict) -> str:
 
 
 def get_bills():
-    bills = {}
+    seen_bills = set()
 
     # Iterate through each vote folder under 2025
     for folder in CONGRESS_DATA_DIR.iterdir():
@@ -61,10 +58,18 @@ def get_bills():
 
         # Build filter string
         bill_id = build_bill_id(bill)
+
+        # Don't fetch duplicates
+        if bill_id in seen_bills:
+            continue
+
         print(f"Fetching bill status for {bill_id}")
 
         # Call govinfo downloader
         fetch_bill_status(bill_id, bill["congress"])
+
+        seen_bills.add(bill_id)
+
 
 if __name__ == "__main__":
     get_bills()
